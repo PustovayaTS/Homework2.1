d057093fd56ad6b210acd6f17be1f6f56465e59a
PustovayaTS <ais_ta@bk.ru>
2022-09-12 22:24:40 +0700

    Задание 3.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int age = 3;
+        if (age < 5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        }
+        else if (age >= 5 && age < 14) {
+            System.out.println("Ребенок может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
+        } else {
+            System.out.println("Ребенок может кататься без сопровождения взрослого");
+        }
     }
 }
