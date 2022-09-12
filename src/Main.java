5aace183e480aec002ca54e58eaf9b5df5c51bb2
PustovayaTS <ais_ta@bk.ru>
2022-09-12 21:24:45 +0700

    Задание 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
+        int age = 3;
+        if (age >= 7 && age <18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age >= 18 && age < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age < 7) {
+            System.out.println("Ребенок еще не ходит в школу");
+        }
+        else {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
     }
 }
