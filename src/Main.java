7f00b906f5356eee09cc29b1475618f2c8761d94
PustovayaTS <ais_ta@bk.ru>
2022-09-12 22:45:22 +0700

    Задание 3.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        int one = 10;
+        int two = 20;
+        int free = 9;
+        if (one > two && one > free) {
+            System.out.println("Самое большое число = " + one);
+        }
+        else if (two > one && two > free) {
+            System.out.println("Самое большое число = " + two);
+        }
+        else {
+            System.out.println("Самое большое число = " + free);
+        }
     }
 }
