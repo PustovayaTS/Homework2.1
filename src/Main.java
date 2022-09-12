6916a588e7108df4724f7262cd0ebd24914d559b
PustovayaTS <ais_ta@bk.ru>
2022-09-12 21:38:24 +0700

    Задание 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int passengers = 35;
+        if (passengers <= 60) {
+            System.out.println("В вагоне есть свободные сидячие и стоячие места");
+        }
+        else if (passengers > 60 && passengers <= 102) {
+            System.out.println("В вагоне есть свободные стоячие места");
+        } else {
+            System.out.println("В вагоне нет свободных места");
+        }
     }
 }
