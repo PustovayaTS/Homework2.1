60a4f6ec9b57c28d9116cf06b6f88be40cc0cc28
PustovayaTS <ais_ta@bk.ru>
2022-09-12 23:04:11 +0700

    Задание 2.2 исправление

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 16;
+        int age = 5;
         if (age >= 7 && age <18) {
             System.out.println("Ребенок ходит в школу");
         }
         else if (age >= 18 && age < 24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет");
         }
-        else if (age < 7) {
-            System.out.println("Ребенок еще не ходит в школу");
-        }
-        else {
+        else if (age >=24) {
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
     }
