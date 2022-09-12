b9d4a63379348c0b041d10742e0f6deccddaccc5
PustovayaTS <ais_ta@bk.ru>
2022-09-12 21:16:40 +0700

    Задание 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        } else  {
+            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать.");
+        }
     }
 }
